650f00784f726d38fd87620f1c54045425c74f8b
Jaeah Lee <hayanz@snu.ac.kr>
2021-08-25 16:38:01 +0900

    find the face part and crop the image

--- face_crop.py
+++ face_crop.py
@@ -1,13 +1,20 @@
+import random
 import cv2
 import dlib
 
+# import the custom module to pick an image to use
+from pick_samples import *
 
+
+# class to detect the face part and crop
 class Face(object):
     def __init__(self, filename):
         self.filename = filename
         self.img = cv2.imread(filename)
         self.grey = self.make_grayscale()
         self.detector = dlib.get_frontal_face_detector()
+        # http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+        self.predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
     def make_grayscale(self):
         temp = cv2.cvtColor(self.img, cv2.COLOR_RGB2BGR)
@@ -16,13 +23,22 @@ class Face(object):
     # detect the face
     def find_face(self):
         img, grey = self.img, self.grey
-        faces = self.detector(self.img)
+        faces = self.detector(self.grey)
+
+        # marks for detecting the face with the predictor
+        nose_mark, jaw_mark = 29, 8
 
         for face in faces:
             x1, x2 = face.left(), face.right()
             y1, y2 = face.top(), face.bottom()
-            # draw a rectangle to show a border of the face
-            # cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            # raise exception if all coordinates are not positive
+            if x1 < 0 or x2 < 0 or y1 < 0 or y2 < 0:
+                raise UnboundLocalError
+
+            facemarks = self.predictor(image=grey, box=face)
+            # try calculating the length of the lower face
+            # the UnboundLocalError would be raised if calculation fails
+            lowerlength = abs(facemarks.part(nose_mark).y - facemarks.part(jaw_mark).y)
 
         # cv2.imshow(winname="found faces", mat=img)  # for debugging
         # cv2.waitKey(3000)  # wait for 3 seconds
@@ -32,29 +48,36 @@ class Face(object):
 
     # crop the face image
     def crop(self):
-        try:
-            # find coordinates of the border of the face
-            faces = self.find_face()
-            if faces is None or len(faces) == 0:
-                raise UnboundLocalError
-        except UnboundLocalError:
-            print("Cannot crop the face")  # error message for debugging
-            return  # the function ends and return nothing
+        # find coordinates of the border of the face
+        faces = self.find_face()
+        if faces is None or len(faces) == 0:
+            raise UnboundLocalError
 
         # crop the image
         x1, x2, y1, y2 = faces
         cropped = self.img[y1:y2, x1:x2]
 
-        cv2.imshow(winname="cropped faces", mat=cropped)  # for debugging
-        cv2.waitKey(3000)  # wait for 3 seconds
-        cv2.destroyAllWindows()  # close all windows
+        # cv2.imshow(winname="cropped faces", mat=cropped)  # for debugging
+        # cv2.waitKey(3000)  # wait for 3 seconds
+        # cv2.destroyAllWindows()  # close all windows
 
         return cropped
 
 
-# for testing the code with some images
+# to crop the face part and save as a different file
 if __name__ == "__main__":
-    for i in range(5):
-        target = "faces_dataset/sample_" + str(i) + ".jpg"
-        tester = Face(target)
-        tester.crop()
+    # path of images
+    before_dir = "faces_dataset/picked/"
+    after_dir = "faces_dataset/picked_cropped/"
+    # index of starting point and endpoint
+    start, end = 500, 1000
+    for i in range(start, end):
+        while True:
+            try:
+                target = Face(before_dir + str(i) + ".jpg")
+                cv2.imwrite(after_dir + str(i) + ".jpg", target.crop())
+                print("[%d] Successfully saved!" % i)
+                break  # the loop ends if an image is successfully saved
+            except (UnboundLocalError, ValueError, cv2.error):
+                dir_chosen = random.choice([dir1, dir2])
+                move_files(dir_chosen, end=1, add_num=i)
