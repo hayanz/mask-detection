40b0436eeaae3f0c857121978801e08cd691e1f9
Jaeah Lee <hayanz@snu.ac.kr>
2021-08-25 16:37:16 +0900

    detection model based on CNN(not implemented yet)

--- add_mask.py
+++ add_mask.py
@@ -13,7 +13,7 @@ from face_crop import Face  # to detect the face part and crop the image
 # colors of the mask
 MASKS = ["white.png", "black.png", "black_grey.png", "white_grey.png", "grey.png"]
 # number of images to fix
-COUNT = 1000
+COUNT = 1
 
 # path of the images
 target_dir = "faces_dataset/picked_mask_cropped/"
@@ -21,43 +21,58 @@ mask_dir = "mask_images/"
 
 start_idx = 0
 
-for i in range(start_idx, COUNT):
-    while True:
-        try:
-            # define the class to access the image
-            person = Person(target_dir + str(i) + ".jpg")  # class of the person image
-            # get the value of the length of the lower face to resize the mask image
-            length = int(person.lower_length)
 
-            # crop the face part
-            # cropped = Face(person.img).crop()  # TODO erase '#'
+# to crop the image and save
+def save_cropped(filename):
+    try:
+        target = Face(filename)
+        cropped = target.crop()
+        cv2.imwrite(filename, cropped)
+        return True
+    except (UnboundLocalError, ValueError):
+        return False
 
-            # pick the color of the mask and define the class to access the image
-            img = person.img.convert("RGBA")
-            # img = cropped.convert("RGBA")  # TODO erase '#'
-            # maskfile = mask_dir + random.choice(MASKS)
-            maskfile = mask_dir + "white.png"
-            mask = Mask(maskfile)  # class of the mask image
 
-            # calculate the size with the ratio of the mask image
-            size = int(round(length * mask.get_ratio()))
-            mask.resize(size, size)  # resized
+if __name__ == "__main__":
+    for i in range(start_idx, COUNT):
+        while True:
+            try:
+                filename = target_dir + str(i) + ".jpg"
+                # define the class to access the image
+                person = Person(filename)  # class of the person image
+                # get the value of the length of the lower face to resize the mask image
+                length = int(person.lower_length)
 
-            # calculate the coordinate of the center again with the size of the mask image
-            mask_center = mask.find_center()
-            coordinate = tuple(int(a - b) for a, b in zip(person.find_center(), mask_center))
-            # paste the mask image to the original face image
-            combined = person.put_mask(mask.img, coordinate)
-            combined = combined.convert("RGB")
-            # combined.show()  # for debugging
-            combined.save(target_dir + str(i) + ".jpg")
-            print("[%d] Successfully Done!" % i)  # for debugging
-            break  # the infinite loop ends if the mask image is pasted
+                # crop the face part and save the result
+                success_crop = save_cropped(filename)
+                if not success_crop:
+                    raise UnboundLocalError
+                # reload the image
+                person = Person(filename)
 
-        except (UnboundLocalError, ValueError):
-            # UnboundLocalError: when the face cannot be found
-            # ValueError: when the pixel of top and bottom are selected in duplicate
-            pick_dir = random.choice([dir1, dir2])  # choose a directory randomly
-            move_files(dir2, end=1, add_num=i)  # pick one picture randomly again
+                # pick the color of the mask and define the class to access the image
+                img = person.img.convert("RGBA")
+                # maskfile = mask_dir + random.choice(MASKS)
+                maskfile = mask_dir + "white.png"
+                mask = Mask(maskfile)  # class of the mask image
 
-# TODO crop the face part of the image and paste a mask image, then save as the training data
+                # calculate the size with the ratio of the mask image
+                size = int(round(length * mask.get_ratio()))
+                mask.resize(size, size)  # resized
+
+                # calculate the coordinate of the center again with the size of the mask image
+                mask_center = mask.find_center()
+                coordinate = tuple(int(a - b) for a, b in zip(person.find_center(), mask_center))
+                # paste the mask image to the original face image
+                combined = person.put_mask(mask.img, coordinate)
+                combined = combined.convert("RGB")
+                # combined.show()  # for debugging
+                combined.save(filename)
+                print("[%d] Successfully Done!" % i)  # for debugging
+                break  # the infinite loop ends if the mask image is pasted
+
+            except (UnboundLocalError, ValueError):
+                # UnboundLocalError: when the face cannot be found
+                # ValueError: when the pixel of top and bottom are selected in duplicate
+                pick_dir = random.choice([dir1, dir2])  # choose a directory randomly
+                move_files(dir2, end=1, add_num=i)  # pick one picture randomly again
