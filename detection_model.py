20d6ae9481fcb2cd00f95723fd9ae27eeb6e59ea
Jaeah Lee <hayanz@snu.ac.kr>
2021-08-30 21:51:05 +0900

    model based on ResNet

--- detection_model.py
+++ detection_model.py
@@ -1,78 +1,74 @@
 """
 The following custom class is based on ResNet;
 Reference:
-[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun,
+    Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun,
     Deep Residual Learning for Image Recognition
 """
 
+# base code: https://github.com/kuangliu/pytorch-cifar/blob/master/models/resnet.py
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-EXPANSION = 4
 
-
-class SoftmaxClassifier(nn.Module):
+# class of the classifier based on softmax regression
+class Classifier(nn.Module):
     def __init__(self, in_dim, out_dim):
-        super(SoftmaxClassifier, self).__init__()
+        super(Classifier, self).__init__()
         self.linear = nn.Linear(in_dim, out_dim)
 
     def forward(self, x):
         return self.linear(x)
 
 
-# class of the basic block based on deeper bottleneck block
-class BaseBlock(nn.Module):
+# class of the basic block based on residual learning
+class ResBlock(nn.Module):
     def __init__(self, input_planes, planes, stride=1):
-        super(BaseBlock, self).__init__()
-        self.expansion = EXPANSION
-        self.conv1 = nn.Conv2d(input_planes, planes, kernel_size=(1, 1), bias=False)
+        super(ResBlock, self).__init__()
+        self.conv1 = nn.Conv2d(input_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)  # batch normalization
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=(3, 3), stride=stride,
-                               padding=1, bias=False)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)  # batch normalization
-        self.conv3 = nn.Conv2d(planes, self.expansion * planes, kernel_size=(1, 1), bias=False)
-        self.bn3 = nn.BatchNorm2d(planes)  # batch normalization
 
         self.shortcut = nn.Sequential()
         # deeper residual function
-        if stride != 1 or input_planes != self.expansion * planes:
+        if stride != 1 or input_planes != planes:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(planes, self.expansion * planes, kernel_size=(1, 1), stride=1, bias=False),
-                nn.BatchNorm2d(self.expansion * planes))
+                nn.Conv2d(input_planes, planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes)  # batch normalization
+            )
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
-        out = F.relu(self.bn2(self.conv2(out)))
-        out = self.bn3(self.conv3(out))
+        out = self.bn2(self.conv2(out))
         out += self.shortcut(x)  # for residual learning
         out = F.relu(out)
         return out
 
 
-# class of the custom CNN based on ResNet (50/101/152 ResNet)
+# class of the custom CNN based on ResNet (used in 18/34)
+# the number of blocks is not fixed (to modify the model more easily)
 class Net(nn.Module):
-    def __init__(self, num_blocks, num_classes=2):
+    def __init__(self, num_blocks, num_classes=1):
         super(Net, self).__init__()
-        self.input_planes = 64
-        self.expansion = EXPANSION
-        assert type(num_blocks) == list
+        self.input_planes = 16
+        assert type(num_blocks) in (list, tuple) and len(num_blocks) == 4
         self.first, self.second, self.third, self.fourth = num_blocks
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)  # batch normalization
-        self.layer1 = self.__custom_layer(64, self.first, stride=1)
-        self.layer2 = self.__custom_layer(128, self.second, stride=2)
-        self.layer3 = self.__custom_layer(256, self.third, stride=2)
-        self.layer4 = self.__custom_layer(512, self.fourth, stride=2)
-        self.softmax = SoftmaxClassifier(512 * self.expansion, num_classes)
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(16)  # batch normalization
+        self.layer1 = self.__custom_layer(16, self.first, stride=1)
+        self.layer2 = self.__custom_layer(32, self.second, stride=2)
+        self.layer3 = self.__custom_layer(64, self.third, stride=2)
+        self.layer4 = self.__custom_layer(128, self.fourth, stride=2)
+        self.classifier = Classifier(128, num_classes)
 
     def __custom_layer(self, planes, num_blocks, stride):
         strides = [stride] + [1] * (num_blocks - 1)
         layers = []
         for stride in strides:
-            layers.append(BaseBlock(self.input_planes, planes, stride=stride))
-            self.input_planes = planes * self.expansion
+            layers.append(ResBlock(self.input_planes, planes, stride=stride))
+            self.input_planes = planes
         return nn.Sequential(*layers)
 
     def forward(self, x):
@@ -81,7 +77,14 @@ class Net(nn.Module):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
+        out = F.avg_pool2d(out, 16)  # average pool
         out = out.view(out.size(0), -1)  # flatten
-        out = self.softmax(out)
+        out = self.classifier(out)
         return out
+
+
+# for debugging
+if __name__ == "__main__":
+    net = Net([3, 4, 6, 3])
+    y = net(torch.rand([1, 3, 128, 128]))
+    print(y.size())
