58114729abd8006c79d8be21456a076f8596fd63
Jaeah Lee <hayanz@snu.ac.kr>
2021-08-30 21:51:42 +0900

    make a dataset to train the model

--- dataset_image.py
+++ dataset_image.py
@@ -1,13 +1,21 @@
-import csv
+import torch
+import torch.cuda.random
 from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms
+from sklearn.model_selection import train_test_split
 
+import csv
 import pandas as pd
+
+master_path = "datasets/"
+torch.manual_seed(1051325718700006030)  # 1051325718700006030: initial random seed
+if torch.cuda.is_available() == 'cuda':
+    torch.cuda.random.manual_seed_all(6763088558125263)  # 6763088558125263: initial random seed of cuda
 
 
 class ImageTransform:
-    def __init__(self, size, mean, std):
+    def __init__(self, size, mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)):
         assert type(size) == tuple
         self.data_transform = transforms.Compose([
             transforms.Resize(size),
@@ -43,31 +51,51 @@ def make_csv(filename):
     positive_path = "positive/"  # faces with mask
     negative_path = "negative/"  # faces without mask
     filetype = ".jpg"
+    num_all = 1000
 
-    positive = [positive_path + str(i) + filetype for i in range(1000)]
-    negative = [negative_path + str(i) + filetype for i in range(1000)]
+    positive = [positive_path + str(i) + filetype for i in range(num_all)]
+    negative = [negative_path + str(i) + filetype for i in range(num_all)]
 
-    filepath = "face_images/" + filename + ".csv"
+    filepath = master_path + filename + ".csv"
     with open(filepath, "w") as file:
         writer = csv.writer(file)
         writer.writerow(["filename", "label"])
-        for i in range(1000):
+        for i in range(num_all):
             writer.writerow([positive[i], 1])
             writer.writerow([negative[i], 0])
 
     return filepath
 
 
+# to divide the columns in csv file
+def separate_csv(filename):
+    datalist = pd.read_csv(filename)
+    img = [master_path + path for path in datalist['filename']]
+    labels = [label for label in datalist['label']]
+    return img, labels
+
+
+# to divide data to train set and test set
+def divide_sets(dataset, ratio):
+    X, y = dataset.X, dataset.y
+    transform = dataset.transform
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=ratio, shuffle=True, random_state=1004)
+    trainset = ImgDataset(transform, X_train, y_train)
+    testset = ImgDataset(transform, X_test, y_test)
+    return trainset, testset
+
+
 # for debugging
 if __name__ == "__main__":
-    master_path = "face_images/"
-    datalist = pd.read_csv(make_csv("dataset"))
-    print(datalist.head())  # for debugging
-    print(datalist['label'].value_counts())  # for debugging
+    # datalist = pd.read_csv(make_csv("dataset"))
+    # print(datalist.head())  # for debugging
+    # print(datalist['label'].value_counts())  # for debugging
 
-    train_img = [master_path + path for path in datalist['filename']]
-    train_labels = datalist['label']
+    images, labels = separate_csv(make_csv("dataset"))
 
-    size, one_half = (100, 100), (0.5, 0.5, 0.5)
-    custom_transform = ImageTransform(size, mean=one_half, std=one_half)
-    trains = ImgDataset(custom_transform, train_img, labels=train_labels)
+    size = (100, 100)
+    custom_transform = ImageTransform(size)
+    data = ImgDataset(custom_transform, images, labels=labels)
+    train, test = divide_sets(data, 0.3)
+    print(train[0])  # for debugging
+    print(test[0])  # for debugging
