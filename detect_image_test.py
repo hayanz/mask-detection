ecf1d2e1d3638f7401ddbe63777bc2a751da193f
Jaeah Lee <hayanz@snu.ac.kr>
2021-09-06 22:18:52 +0900

    est the model with images

--- detect_image_test.py
+++ detect_image_test.py
@@ -5,13 +5,17 @@ import torch.nn as nn
 import cv2
 from PIL import Image
 
-# import the custom module
-from detector.detection_model import Net
+# import custom modules
+from detector.model_cnn import Net
 from modify_images.face_crop import Face
 from detector.dataset_image import ImageTransform
 
-detector_path = "checkpoint/saved_1.pth"
-classes = {
+# best model of main_ckpt: best_1.pth
+# best model of additional_ckpt: saved_seocnd.pth
+# TODO fine the status of the best model of each trial (epoch, accuracy, loss, ... etc.)
+# detector_path = "main_ckpt/best_1.pth"
+detector_path = "additional_ckpt/saved_second.pth"
+classifier = {
     0: 'no_mask',
     1: 'mask'
 }
@@ -49,6 +53,7 @@ def convert_image(imgpath, crop=True):
     return image_pil
 
 
+# to detect the image with a trained model
 def detection(image, detector):
     transform = ImageTransform(size=(128, 128))
     image = torch.unsqueeze(transform(img=image), 0)  # transform an image for detecting
@@ -56,16 +61,32 @@ def detection(image, detector):
     return result
 
 
+# to classify the image with the result
+def classify(prob):
+    if prob <= 0:
+        return classifier[0], 100
+    elif prob >= 1:
+        return classifier[1], 100
+    elif prob < 0.5:
+        accuracy = 100 * (1 - prob)
+        return classifier[0], accuracy
+    else:
+        accuracy = 100 * prob
+        return classifier[1], accuracy
+
+
 if __name__ == "__main__":
     print("without mask...")
     for i in range(4):
-        test_path = "face_images/sample_%d.jpg" % i
+        test_path = "test_images/sample_%d.jpg" % i
         test_image = convert_image(test_path, crop=True)
         detector = load_detector(USE_CUDA)
-        print(detection(image=test_image, detector=detector).item())
+        result = classify(detection(image=test_image, detector=detector).item())
+        print(result)
     print("with mask...")
-    for i in range(2):
-        test_path_1 = "face_images/mask_cropped_%d.jpg" % i
+    for i in range(12):
+        test_path_1 = "test_images/mask_cropped/%d.jpg" % i
         test_image_1 = convert_image(test_path_1, crop=False)
         detector = load_detector(USE_CUDA)
-        print(detection(image=test_image_1, detector=detector).item())
+        result = classify(detection(image=test_image_1, detector=detector).item())
+        print(result)
